9c98e84998e348437671564da1ef4bfadcbc82d6
FernanGon21 <fernando27254@gmail.com>
2025-04-04 20:45:29 -0400

    Delete index.jsx

--- index.jsx
+++
@@ -1,7 +0,0 @@
-// Tienda de bebidas con personalización desde admin
-import { useState, useEffect } from "react";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Card, CardContent } from "@/components/ui/card";
-
-// ... (rest of the code omitted for brevity, this will be replaced in actual output)
